123bd24120a20e3e39751e41e23218bc7a6d40bb
pascmartin <pascmartin>
2003-03-17 01:42:41 +0000

    Reorganize the command line option code, add a --help and documentation.

--- src/roadmap_option.c
+++ src/roadmap_option.c
@@ -86,80 +86,205 @@ int roadmap_option_height (char *name) {
 }
 
 
+static void roadmap_option_set_location (const char *value) {
+
+    roadmap_config_set ("Locations", "Location", value);
+}
+
+
+static void roadmap_option_set_metric (const char *value) {
+
+    roadmap_config_set ("General", "Unit", "metric");
+}
+
+
+static void roadmap_option_set_imperial (const char *value) {
+
+    roadmap_config_set ("General", "Unit", "imperial");
+}
+
+
+static void roadmap_option_set_no_area (const char *value) {
+
+    roadmap_option_no_area = 1;
+}
+
+
+static void roadmap_option_set_geometry1 (const char *value) {
+
+    roadmap_config_set ("Geometry", "Main", value);
+}
+
+
+static void roadmap_option_set_geometry2 (const char *value) {
+
+    char *p;
+    char *geometry;
+    char buffer[256];
+          
+    strncpy (buffer, value, sizeof(buffer));
+          
+    geometry = strchr (buffer, '=');
+    if (geometry == NULL) {
+        roadmap_log (ROADMAP_FATAL,
+                     "%s: invalid geometry option syntax", value);
+    }
+    *(geometry++) = 0;
+         
+    for (p = strchr(buffer, '-'); p != NULL; p =strchr(p, '-')) {
+        *p = ' ';
+    }
+         
+    roadmap_config_declare ("preferences", "Geometry", buffer, "300x200");
+    roadmap_config_set ("Geometry", buffer, geometry);
+}
+
+
+static void roadmap_option_set_no_toolbar (const char *value) {
+
+    roadmap_config_set ("General", "Toolbar", "no");
+}
+
+
+static void roadmap_option_set_square (const char *value) {
+
+    roadmap_option_square = 1;
+}
+
+
+static void roadmap_option_set_gps (const char *value) {
+
+    if (roadmap_option_gps != NULL) {
+        free (roadmap_option_gps);
+    }
+    roadmap_option_gps = strdup (value);
+}
+
+
+static void roadmap_option_set_debug (const char *value) {
+
+    if (roadmap_option_verbose > ROADMAP_MESSAGE_DEBUG) {
+        roadmap_option_verbose = ROADMAP_MESSAGE_DEBUG;
+    }
+}
+
+
+static void roadmap_option_set_verbose (const char *value) {
+
+    if (roadmap_option_verbose > ROADMAP_MESSAGE_INFO) {
+        roadmap_option_verbose = ROADMAP_MESSAGE_INFO;
+    }
+}
+
+
+static void roadmap_option_usage (const char *value);
+
+
+typedef void (*roadmap_option_handler) (const char *value);
+
+struct roadmap_option_descriptor {
+    
+    const char *name;
+    const char *format;
+    
+    roadmap_option_handler handler;
+    
+    const char *help;
+};
+
+static struct roadmap_option_descriptor RoadMapOptionMap[] = {
+    
+    {"--location=", "LONGITUDE,LATITUDE", roadmap_option_set_location,
+        "Set the location point (see menu entry Screen/Show Location..)"},
+
+    {"--metric", "", roadmap_option_set_metric,
+        "Use the metric system for all units"},
+
+    {"--imperial", "", roadmap_option_set_imperial,
+        "Use the imperial system for all units"},
+
+    {"--no-area", "", roadmap_option_set_no_area,
+        "Do not show the polygons (parks, hospitals, airports, etc..)"},
+
+    {"-geometry=", "WIDTHxHEIGHT", roadmap_option_set_geometry1,
+        "Same as the --geometry option below"},
+
+    {"--geometry=", "WIDTHxHEIGHT", roadmap_option_set_geometry1,
+        "Set the geometry of the RoadMap main window"},
+
+    {"--geometry:", "WINDOW=WIDTHxHEIGHT", roadmap_option_set_geometry2,
+        "Set the geometry of a specific RoadMap window"},
+
+    {"--no-toolbar", "", roadmap_option_set_no_toolbar,
+        "Hide the RoadMap main window's toolbar"},
+
+    {"--square", "", roadmap_option_set_square,
+        "Show the square boundaries as grey lines (for debug purpose)"},
+
+    {"--gps=", "URL", roadmap_option_set_gps,
+        "Use a specific GPS source (mainly for replay of a GPS log)"},
+
+    {"--debug", "", roadmap_option_set_debug,
+        "Show all informational and debug traces"},
+
+    {"--verbose", "", roadmap_option_set_verbose,
+        "Show all informational traces"},
+
+    {"--help", "", roadmap_option_usage,
+        "Show this help message"},
+
+    {NULL, NULL, NULL, NULL}
+};
+
+
+static void roadmap_option_usage (const char *value) {
+
+    struct roadmap_option_descriptor *option;
+
+    for (option = RoadMapOptionMap; option->name != NULL; ++option) {
+
+        printf ("  %s%s\n", option->name, option->format);
+        printf ("        %s.\n", option->help);
+    }
+    exit(0);
+}
+
+
 void roadmap_option (int argc, char **argv) {
 
-   int i;
+    int   i;
+    int   length;
+    int   compare;
+    char *value;
+    struct roadmap_option_descriptor *option;
 
-   for (i = 1; i < argc; i++) {
 
-      if (strncmp (argv[i], "--location=", 11) == 0) {
+    for (i = 1; i < argc; i++) {
 
-         roadmap_config_set ("Locations", "Location", argv[i] + 11);
+        compare = 1; /* Different. */
+        
+        for (option = RoadMapOptionMap; option->name != NULL; ++option) {
 
-      } else if (strcmp (argv[i], "--metric") == 0) {
+            if (option->format[0] == 0) {
 
-         roadmap_config_set ("General", "Unit", "metric");
+                value = NULL;
+                compare = strcmp (option->name, argv[i]);
 
-      } else if (strcmp (argv[i], "--imperial") == 0) {
+            } else {
+                length = strlen (option->name);
+                value = argv[i] + length;
+                compare = strncmp (option->name, argv[i], length);
+            }
 
-         roadmap_config_set ("General", "Unit", "imperial");
-
-      } else if (strcmp (argv[i], "--no-area") == 0) {
-
-         roadmap_option_no_area = 1;
-
-      } else if (strncmp (argv[i], "--geometry=", 11) == 0) {
-
-         roadmap_config_set ("Geometry", "Main", argv[i]+11);
-
-      } else if (strncmp (argv[i], "--geometry:", 11) == 0) {
-
-         char *p;
-         char *value;
-         char buffer[256];
-          
-         strncpy (buffer, argv[i]+11, sizeof(buffer));
-          
-         value = strchr (buffer, '=');
-         if (value == NULL) {
-            roadmap_log (ROADMAP_FATAL, "%s: invalid option syntax", argv[i]);
-         }
-         *(value++) = 0;
-         
-         for (p = strchr(buffer, '-'); p != NULL; p =strchr(p, '-')) {
-             *p = ' ';
-         }
-         
-         roadmap_config_declare ("preferences", "Geometry", buffer, "300x200");
-         roadmap_config_set ("Geometry", buffer, value);
-
-      } else if (strcmp (argv[i], "--no-toolbar") == 0) {
-
-         roadmap_config_set ("General", "Toolbar", "no");
-
-      } else if (strcmp (argv[i], "--square") == 0) {
-
-         roadmap_option_square = 1;
-
-      } else if (strncmp (argv[i], "--gps=", 6) == 0) {
-
-         roadmap_option_gps = argv[i] + 6;
-
-      } else if (strcmp (argv[i], "--debug") == 0) {
-
-         if (roadmap_option_verbose > ROADMAP_MESSAGE_DEBUG) {
-            roadmap_option_verbose = ROADMAP_MESSAGE_DEBUG;
-         }
-
-      } else if (strcmp (argv[i], "--verbose") == 0) {
-
-         if (roadmap_option_verbose > ROADMAP_MESSAGE_INFO) {
-            roadmap_option_verbose = ROADMAP_MESSAGE_INFO;
-         }
-
-      } else {
-         roadmap_log (ROADMAP_FATAL, "illegal option %s", argv[i]);
-      }
-   }
+            if (compare == 0) {
+                option->handler (value);
+                break;
+            }
+        }
+        
+        if (compare != 0) {
+            roadmap_log (ROADMAP_FATAL, "illegal option %s", argv[i]);
+        }
+    }
 }
 
