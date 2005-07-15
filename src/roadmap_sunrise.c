5441b4283578f240ec68bbad67c1bc0cd760d442
pascmartin <pascmartin>
2005-07-15 20:02:18 +0000

    Make it generate reasonable result for LA, Paris, London

--- src/roadmap_sunrise.c
+++ src/roadmap_sunrise.c
@@ -47,15 +47,15 @@
 
 static double roadmap_sunrise_getrange(double x) {
 
-	double temp1,temp2;
+   double temp1,temp2;
 
-	temp1 = x / (2 * PI);
-	temp2 = (2 * PI) * (temp1 - floor(temp1));
-	if(temp2<0) {
+   temp1 = x / (2 * PI);
+   temp2 = (2 * PI) * (temp1 - floor(temp1));
+   if(temp2<0) {
        temp2 = (2*PI) + temp2;
    }
 
-	return(temp2);
+   return(temp2);
 }
 
 
@@ -132,19 +132,19 @@ static time_t roadmap_sunrise_getriseorset
 
    double sinalt,sinphi,cosphi;
    double longitude,l,gx,gha,correction,ec,lambda,delta,e,obl,cosc;
-	double altitude,ephem2000day,utold,utnew;
+   double utold,utnew;
    double days,t;
-	int count = 0;
-   double roadmap_long = LU_TO_DEG(position->longitude);
-	double roadmap_lat  = LU_TO_DEG(position->latitude);
+   int count = 0;
+   int ephem2000day;
+   double roadmap_lat;
 
-	/* not accurate for places too close to the poles */
-	if(roadmap_lat > 63 || roadmap_lat < -63) {
+   /* not accurate for places too close to the poles */
+   if(abs(position->latitude) > 63000000) {
        return -1;
-	}
+   }
 
-	/* check for valid riseorset parameter */
-	if((riseorset!=1)&&(riseorset!=-1)) {
+   /* check for valid riseorset parameter */
+   if((riseorset!=1)&&(riseorset!=-1)) {
        return -1;
    }
 
@@ -153,27 +153,27 @@ static time_t roadmap_sunrise_getriseorset
    curtime_gmt = *(gmtime(&gmt));
 
 
-	altitude = -0.833; /* tbd: sets according to position->altitude. */
+   ephem2000day = 367 * (curtime.tm_year+1900)
+                      - 7 * ((curtime.tm_year+1900)
+                                  + ((curtime.tm_mon+1) + 9) / 12) / 4
+                      + 275 * (curtime.tm_mon+1) / 9
+                      + curtime.tm_mday - 730531;
+   utold = PI;
+   utnew = 0;
+   
+   sinalt = -0.0174456; /* tbd: sets according to position->altitude. */
 
-	ephem2000day = 367.0 * (curtime.tm_year+1900)
-                      - 7.0 * ((curtime.tm_year+1900)
-                                  + ((curtime.tm_mon+1) + 9.0) / 12.0) / 4.0
-                      + 275.0 * (curtime.tm_mon+1) / 9.0
-                      + curtime.tm_mday - 730531.5;
-	utold = PI;
-	utnew = 0;
-	
-   sinalt = sin(altitude * RADIANS);
-	sinphi = sin(roadmap_lat * RADIANS);
-	cosphi = cos(roadmap_lat * RADIANS);
-	longitude = (roadmap_long * RADIANS);
+   roadmap_lat  = LU_TO_DEG(position->latitude);
+   sinphi = sin(roadmap_lat * RADIANS);
+   cosphi = cos(roadmap_lat * RADIANS);
+   longitude = LU_TO_DEG(position->longitude) * RADIANS;
 
-	while((fabs(utold-utnew) > 0.001) && (count < 35))  {
+   while((fabs(utold-utnew) > 0.001) && (count < 35))  {
 
       count++;
-		utold=utnew;
-		days = ephem2000day + utold / (2 * PI);
-		t = days / 36525;
+      utold=utnew;
+      days = (1.0 * ephem2000day - 0.5) + utold / (2 * PI);
+      t = days / 36525;
       l  = roadmap_sunrise_getrange(4.8949504201433 + 628.331969753199 * t);
       gx = roadmap_sunrise_getrange(6.2400408 + 628.3019501 * t);
       ec = .033423 * sin(gx) + .00034907 * sin(2. * gx);
@@ -186,21 +186,27 @@ static time_t roadmap_sunrise_getriseorset
 
       gha = utold - PI + e;
       cosc = (sinalt - sinphi * sin(delta)) / (cosphi * cos(delta));
-		if(cosc > 1) {
+      if(cosc > 1) {
          correction = 0;
       } else if(cosc < -1) {
-			correction = PI;
+         correction = PI;
       } else {
-			correction = atan((sqrt(1 - cosc * cosc)) / cosc);
+         correction = atan((sqrt(1 - cosc * cosc)) / cosc);
       }
 
-		utnew = roadmap_sunrise_getrange
+      utnew = roadmap_sunrise_getrange
                   (utold - (gha + longitude + riseorset * correction));
    }
 
-	/* utnew must now be converted into gmt. */
+   /* utnew must now be converted into gmt. */
 
    result = roadmap_sunrise_get_gmt (utnew * DEGREES / 15, &curtime_gmt);
+
+   /* I don't know why, but the time seems shifted by 12 hours.
+    * This is not a fix: this is a quick & dirty hack.
+    */
+   result -= (12 * 3600);
+
    if (result < gmt) {
       result += (24 * 3600);
    }
@@ -312,6 +318,7 @@ int main(int argc, char **argv) {
    } else if (argc != 3) {
       fprintf (stderr, "Usage: %s longitude latitude [timezone]\n"
                        "missing position\n", argv[0]);
+      exit(1);
    }
    position.longitude = atoi(argv[1]);
    position.latitude = atoi(argv[2]);
