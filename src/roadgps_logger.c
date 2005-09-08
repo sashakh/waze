f6370ad6116f17f3d05453702bb4ac9605d29ce8
eshabtai <eshabtai>
2005-09-08 06:57:00 +0000

    Use the file selection dialog to get the path and filename of the logger.

--- src/roadgps_logger.c
+++ src/roadgps_logger.c
@@ -32,14 +32,12 @@
 #include "roadmap_gps.h"
 #include "roadmap_path.h"
 #include "roadmap_main.h"
+#include "roadmap_fileselection.h"
 
 #include "roadgps_logger.h"
 
 
 static FILE *RoadGpsOutput = NULL;
-
-static RoadMapConfigDescriptor RoadMapConfigLogPath =
-                      ROADMAP_CONFIG_ITEM("Log", "Path");
 
 
 static void roadgps_logger (const char *sentence) {
@@ -61,51 +59,30 @@ void roadgps_logger_stop (void) {
 }
 
 
-void roadgps_logger_start (void) {
-
-   static unsigned char RoadGpsCounter = 1;
-   static char *RoadGpsName = "roadgps-%d.log";
-
-   char name[80];
-   char *fullname = NULL; /* Avoid spurious warning. */
-
-   const char *path = roadmap_config_get (&RoadMapConfigLogPath);
-
+static void roadgps_logger_file_dialog_ok
+                           (const char *filename, const char *mode) {
 
    roadgps_logger_stop ();
 
-   for (;;) {
-
-      if (RoadGpsCounter <= 0) {
-         RoadGpsCounter = 1;
-         break;
-      }
-      snprintf (name, sizeof(name), RoadGpsName, RoadGpsCounter);
-      fullname = roadmap_path_join (path, name);
-
-      RoadGpsOutput = fopen (fullname, "r");
-
-      if (RoadGpsOutput == NULL) break;
-
-      fclose (RoadGpsOutput);
-      RoadGpsCounter += 1;
-      roadmap_path_free (fullname);
-   }
-
-   snprintf (name, sizeof(fullname), RoadGpsName, RoadGpsCounter);
-   RoadGpsOutput = fopen (fullname, "w");
+   RoadGpsOutput = fopen (filename, "w");
 
    if (RoadGpsOutput == NULL) {
-      roadmap_log (ROADMAP_ERROR, "cannot open log file %s", fullname);
+      roadmap_log (ROADMAP_ERROR, "cannot open log file %s", filename);
    }
-   roadmap_path_free (fullname);
+}
+
+
+void roadgps_logger_start (void) {
+                                
+   roadmap_fileselection_new ("RoadGps Log",
+                              NULL, /* no filter. */
+                              roadmap_path_temporary(),
+                              "w",
+                              roadgps_logger_file_dialog_ok);
 }
 
 
 void roadgps_logger_initialize (void) {
-
-   roadmap_config_declare
-      ("preferences", &RoadMapConfigLogPath, roadmap_path_temporary());
 
    roadmap_gps_register_logger (roadgps_logger);
 }
