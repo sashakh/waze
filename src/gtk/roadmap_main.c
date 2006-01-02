ed84d6b0b234ae3296cd103fea1fa3a78bd4f6d4
pascmartin <pascmartin>
2006-01-02 09:11:22 +0000

    Fix warning

--- src/gtk/roadmap_main.c
+++ src/gtk/roadmap_main.c
@@ -26,6 +26,7 @@
  */
 
 #include <time.h>
+#include <stdlib.h>
 #include <sys/time.h>
 
 #include <gtk/gtk.h>
--- src/gtk2/roadmap_main.c
+++ src/gtk2/roadmap_main.c
@@ -26,6 +26,7 @@
  */
 
 #include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 
