9dfed6cd1a92380287b4a0f1adc181abca751b39
eshabtai <eshabtai>
2006-05-02 12:49:25 +0000

    Add serial enumeration.

--- src/roadmap_serial.h
+++ src/roadmap_serial.h
@@ -29,6 +29,7 @@
 #ifndef INCLUDE__ROADMAP_SERIAL__H
 #define INCLUDE__ROADMAP_SERIAL__H
 
+#define MAX_SERIAL_ENUMS 10
 
 #ifdef _WIN32
 
@@ -53,6 +54,7 @@ RoadMapSerial roadmap_serial_open  (const char *name,
 int   roadmap_serial_read  (RoadMapSerial serial, void *data, int size);
 int   roadmap_serial_write (RoadMapSerial serial, const void *data, int length);
 void  roadmap_serial_close (RoadMapSerial serial);
+const int *roadmap_serial_enumerate (void);
 
 #endif // INCLUDE__ROADMAP_FILE__H
 
