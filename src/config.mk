5cd4a7d22ce98de5e0bbbe875cda8c1470ab91cc
pgf <pgf>
2006-06-26 19:47:49 +0000

    refactored makefiles.  conditional logic now in options.mk, user selections now in config.mk.

---
+++ src/config.mk
@@ -0,0 +1,68 @@
+#
+# config.mk
+#
+# This file controls how RoadMap gets built (at least on unix
+# "Makefile-based" platforms).  Try and keep your customizations
+# here.  You can override these settings from the commandline
+# if you wish, e.g. "make MODE=DEBUG".
+
+# If you change settings in this file, it's quite possible that
+# you'll need to do a "make clean" before trying your build again.
+# (In other words, it may appear to build correctly, and in some
+# cases might, but there are no guarantees.)
+
+# Basic building mode
+MODE =		# blank for "normal" build
+# MODE=DEBUG	# enables -g, disables -O, turns on mtrace leak detection
+# MODE=PROFILE	# sets up for profiling with gprof
+
+# Add any other special local CFLAGS values here
+CFLAGS = 
+
+# Choose a desktop
+# DESKTOP = GTK
+DESKTOP = GTK2
+# DESKTOP = GPE
+# DESKTOP = QT
+# DESKTOP = QPE
+
+# if you select QT or QPE above, you might also want to set
+# QTDIR here:
+QTDIR=/usr
+
+
+# RoadMap uses the "expat" library in order to read and write xml
+# for the GPX format route/track/waypoint files.  If you don't
+# libexpat.a on your system, you can still use RoadMap, but you
+# won't be able to import or export your route and waypoint data.
+EXPAT = YES
+# EXPAT = NO
+
+# RoadMap users in the USA will probably use the Tiger maps from
+# the US Census bureau.  These maps do not requre "shapefile"
+# support when building the rdm format of the maps.  Users in
+# other areas will likely be building maps from other sources,
+# and will probably need shapefile support.
+SHAPEFILES = NO
+# SHAPEFILES = YES
+
+# In order to properly label streets on the map, it's necessary
+# to be able to draw text at arbitrary angles.  RoadMap uses the
+# "Anti-Grain Geometry" library, libagg.a, in order to do this. 
+# If you don't have this library, RoadMap can still do street
+# labels, but they will all be horizontal, and won't look as
+# nice.  (for more information on AGG, see http://antigrain.com)
+# [ Note -- currently, only GTK2 supports AGG builds.  ]
+# AGG = YES
+AGG = NO
+
+# Some languages need to present text that flows from right to to
+# left.  RoadMap supports this via the "Free Implementation of
+# the BiDirectional" algorithm library, (aka "FriBidi" -- for
+# more information, see http://fribidi.org )  (AGG is also
+# required.)
+BIDI = NO
+# BIDI = YES
+
+# RoadMap internal profiling -- you probably don't want this.
+# DBG_TIME = YES
---
+++ src/options.mk
@@ -0,0 +1,144 @@
+
+# Build options for RoadMap.  Mostly this file shouldn't need
+# to be edited.  Personal changes can probably be made in config.mk
+
+include $(TOP)/config.mk
+
+# --- Installation options -------------------------------------------------
+
+DESTDIR=
+INSTALLDIR=/usr/local
+desktopdir=$(INSTALLDIR)/applications
+pkgdatadir=$(INSTALLDIR)/share/roadmap
+pkgmapsdir=/var/lib/roadmap
+bindir=$(INSTALLDIR)/bin
+pkgbindir=$(bindir)
+menudir=$(DESTDIR)/usr/lib/menu
+ICONDIR=$(INSTALLDIR)/share/pixmaps
+mandir=$(INSTALLDIR)/share/man
+man1dir=$(mandir)/man1
+
+INSTALL      = install
+INSTALL_DATA = install -m644
+
+
+# --- Build options ------------------------------------------------
+
+ifeq ($(DESKTOP),GTK2)
+	RDMODULES=gtk2
+else
+ifeq ($(DESKTOP),GPE)
+	RDMODULES=gtk2
+else
+ifeq ($(DESKTOP),GTK)
+	RDMODULES=gtk
+else
+ifeq ($(DESKTOP),QT)
+	RDMODULES=qt
+else
+ifeq ($(DESKTOP),QPE)
+	RDMODULES=qt
+else
+	RDMODULES=gtk gtk2 qt
+endif
+endif
+endif
+endif
+endif
+
+
+RANLIB=ranlib
+STRIP=strip
+
+ifeq ($(strip $(MODE)),DEBUG)
+	# Memory leak detection using mtrace:
+	# Do not forget to set the trace file using the env. 
+	# variable MALLOC_TRACE, then use the mtrace tool to
+	# analyze the output.
+	CFLAGS += -g -DROADMAP_DEBUG_HEAP
+else
+ifeq ($(strip $(MODE)),PROFILE)
+	CFLAGS += -g -pg -fprofile-arcs -g
+	LIBS += -pg
+else
+	CFLAGS += -O2 -ffast-math -fomit-frame-pointer
+endif
+endif
+
+WARNFLAGS = -W \
+	-Wall \
+	-Wno-unused-parameter \
+	-Wcast-align \
+	-Wpointer-arith \
+	-Wreturn-type \
+	-Wsign-compare
+
+CFLAGS += $(WARNFLAGS) 
+
+
+RDMLIBS= $(TOP)/libroadmap.a \
+	$(TOP)/unix/libosroadmap.a  \
+	$(TOP)/gpx/libgpx.a \
+	$(TOP)/libroadmap.a 
+
+LIBS += $(RDMLIBS)
+
+# expat library, for xml GPX format
+ifneq ($(strip $(EXPAT)),NO)
+	LIBS += -lexpat 
+	CFLAGS += -DROADMAP_USES_EXPAT
+endif
+
+# shapefiles for some mapsets
+ifneq ($(strip $(SHAPEFILES)),NO)
+	CFLAGS += -DROADMAP_USE_SHAPEFILES
+	LIBS += -lshp
+endif
+
+
+# later...
+# ifneq ($(NAVIGATE),NO)
+# 	LIBS += -L$(TOP)/dglib -ldgl
+# endif
+
+# each DESKTOP version has a fully-"native" canvas
+# implementation, as well as an agg-based implementation.
+ifeq ($(strip $(AGG)),NO)
+	CANVAS_OBJS = roadmap_canvas.o
+else
+	LIBS += -lagg -lfreetype
+	CFLAGS += -DANGLED_LABELS=1 \
+		-I$(TOP)/agg_support \
+		-I/usr/include/agg2 \
+		-I/usr/local/include/agg2 \
+		-I/usr/include/freetype2
+	CANVAS_OBJS = roadmap_canvas_agg.o \
+		$(TOP)/agg_support/roadmap_canvas.o \
+		$(TOP)/agg_support/agg_font_freetype.o
+endif
+
+# bidirectional text lib
+ifneq ($(strip $(BIDI)),NO)
+	LIBS += -lfribidi
+	CFLAGS += -DUSE_FRIBIDI -I/usr/include/fribidi
+endif
+
+# RoadMap internal profiling
+ifeq ($(strip $(DBG_TIME)),YES)
+	CFLAGS += -DROADMAP_DBG_TIME
+endif
+
+
+HOST=`uname -s`
+ifeq ($(HOST),Darwin)
+	ARFLAGS="r"
+else
+	ARFLAGS="rf"
+endif
+
+
+CFLAGS += -I$(TOP) -I/usr/local/include -DNDEBUG
+
+LIBS := -lpopt -L/usr/local/lib $(LIBS) -lm
+
+CXXFLAGS = $(CFLAGS)
