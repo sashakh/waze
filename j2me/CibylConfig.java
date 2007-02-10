2658c97ee8eda3f483d777fcc65d9f314d3fbea5
eshabtai <eshabtai>
2007-02-10 16:09:36 +0000

    Import main Makefile.

---
+++ j2me/CibylConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Configuration for the Cibyl application. If you want to edit these
+ * settings, copy it to your application directory, edit it and
+ * install it into src/ when compiling your application.
+ */
+class CibylConfig
+{
+    /** Memory size - 0 means use default size (Runtime.freeMemory() / 1.4) */
+    public static int memorySize = 3500000;
+
+    /** Stack size - 8KB by default */
+    public static int stackSize = 8192;
+}
