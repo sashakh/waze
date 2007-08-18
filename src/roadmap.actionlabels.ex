33e734b0a98d443056d5c88123289eb0f96ec7ba
pgf <pgf>
2007-08-18 19:36:09 +0000

    new example roadmap.actionlabels and roadmap.toolbar files

---
+++ src/roadmap.actionlabels.ex
@@ -0,0 +1,32 @@
+#
+# Sample "relabeling" file for RoadMap.
+#
+# The names displayed to represent actions in the RoadMap user
+# interface can be modified with a "roadmap.actionlabels" file. 
+# (RoadGps can use a similar file:  "roadgps.actionlabels".)
+#
+# Each action is renamed using one line of text that contains a)
+# the action name, b) its menu label, c) its toolbar label and d)
+# its "tip", in that order.  Each of these items is separated by
+# one special character.  This separator can be any
+# non-alphanumeric character.
+#
+# Different separators can be used on different lines, but the
+# separator must be the same across an entire line.
+#
+# Omitting one of the items leaves it at its default setting --
+# so these examples will only change the toolbar label, leaving
+# the menu entries and tooltips alone.
+# 
+
+address,,Addr
+gps,,GPS
+destination,,Dest
+startpoint,,Start
+gpsaswaypoint,,GPSwpt
+addaswaypoint,,NewPlace
+starttrip,,Begin
+stoptrip,,Stop
+resumeroute,,RouteSync
+toggleview,,2D/3D
+addroutepointnear,,NewRtPt
---
+++ src/roadmap.toolbar.ex
@@ -0,0 +1,16 @@
+#
+# Sample replacement toolbar for RoadMap.
+#
+# One button will invoke the menu system as a series of popups, and
+# the other buttons will perform specific actions.  The button labels will
+# come from "roadmap.actionlabels".
+# If a "Menus..." button like that below is to be used, it may be
+# desireable to suppress the default "normal" menus.  To do so, create
+# an empty "roadmap.menus" file.
+
+->Menus...
+gps
+destination
+addaswaypoint
+resumeroute
+toggleview
