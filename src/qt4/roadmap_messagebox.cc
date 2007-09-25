7961c3b5931173220113d3615c3e9841f1688f87
pgf <pgf>
2007-09-25 13:12:37 +0000

    enable the messagebox calls.  "seems to be fixed in recent qt4 versions" -- Alex Briosi

--- src/qt4/roadmap_messagebox.cc
+++ src/qt4/roadmap_messagebox.cc
@@ -32,9 +32,9 @@ extern "C" {
 }
 
 void roadmap_messagebox(const char* title, const char* message) {
-   //QMessageBox::information(0, roadmap_start_get_title(title), message)
+   QMessageBox::information(0, roadmap_start_get_title(title), message);
 }
 
 void roadmap_messagebox_wait(const char* title, const char* message) {
-   //QMessageBox::critical(0, roadmap_start_get_title(title), message);
+   QMessageBox::critical(0, roadmap_start_get_title(title), message);
 }
