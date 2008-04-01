7b5b0a126e25a47bd88a19375cf43ef75dde8a5d
pgf <pgf>
2008-04-01 16:56:34 +0000

    supply active satellite count to users of "gps monitor" callback

--- src/roadmap_gps.h
+++ src/roadmap_gps.h
@@ -86,6 +86,7 @@ typedef struct {
 typedef void (*roadmap_gps_monitor) (int reception,
                                      const RoadMapGpsPrecision *precision,
                                      const RoadMapGpsSatellite *satellites,
+                                     int activecount,
                                      int count);
 
 void roadmap_gps_register_monitor (roadmap_gps_monitor monitor);
