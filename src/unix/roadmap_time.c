3c8d80ab789b2d513ff65897cb937936da87ca75
dannybackx <dannybackx>
2009-02-10 19:01:57 +0000

    Include the date

--- src/unix/roadmap_time.c
+++ src/unix/roadmap_time.c
@@ -1,8 +1,8 @@
-/* roadmap_time.c - Manage time information & display.
- *
+/*
  * LICENSE:
  *
  *   Copyright 2002 Pascal F. Martin
+ *   Copyright (c) 2008, Danny Backx.
  *
  *   This file is part of RoadMap.
  *
@@ -19,10 +19,11 @@
  *   You should have received a copy of the GNU General Public License
  *   along with RoadMap; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * SYNOPSYS:
- *
- *   See roadmap_time.h
+ */
+
+/**
+ * @file
+ * @brief unix/roadmap_time.c - Manage time information & display.
  */
 
 #include <string.h>
@@ -34,24 +35,35 @@
 #include "roadmap.h"
 #include "roadmap_time.h"
 
-
+/**
+ * @brief convert time_t into a string
+ * @param gmt the input time
+ * @return the string that can be printed
+ */
 char *roadmap_time_get_hours_minutes (time_t gmt) {
     
-    static char image[16];
+    static char image[32];
     
     struct tm *tm;
     
     tm = localtime (&gmt);
     snprintf (image, sizeof(image), "%2d:%02d", tm->tm_hour, tm->tm_min);
+    snprintf (image, sizeof(image), "%4d.%02d.%02d %2d:%02d",
+		    1900 + tm->tm_year, tm->tm_mon + 1, tm->tm_mday,
+		    tm->tm_hour, tm->tm_min);
 
     return image;
 }
 
-unsigned long tv_to_msec(struct timeval *tv)
+static unsigned long tv_to_msec(struct timeval *tv)
 {
     return (tv->tv_sec & 0xffff) * 1000 + tv->tv_usec/1000;
 }
 
+/**
+ * @brief Get some kind of time indication in milliseconds, on UNIX it's the current time
+ * @return return the time in milliseconds
+ */
 unsigned long roadmap_time_get_millis(void) {
    struct timeval tv;
 
@@ -59,5 +71,3 @@ unsigned long roadmap_time_get_millis(void) {
    return tv_to_msec(&tv);
 
 }
-
-
--- src/win32/roadmap_time.c
+++ src/win32/roadmap_time.c
@@ -1,8 +1,8 @@
-/* roadmap_time.c - Manage time information & display.
- *
+/*
  * LICENSE:
  *
  *   Copyright 2002 Pascal F. Martin
+ *   Copyright (c) 2008, Danny Backx.
  *
  *   This file is part of RoadMap.
  *
@@ -19,32 +19,42 @@
  *   You should have received a copy of the GNU General Public License
  *   along with RoadMap; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * SYNOPSYS:
- *
- *   See roadmap_time.h
+ */
+
+/**
+ * @file
+ * @brief win32/roadmap_time.c - Manage time information & display.
+ * @ingroup windows
  */
 
 #include "../roadmap.h"
 #include "../roadmap_time.h"
 
-
+/**
+ * @brief convert time_t into a string
+ * @param gmt the input time
+ * @return the string that can be printed
+ */
 char *roadmap_time_get_hours_minutes (time_t gmt) {
-    
-    static char image[16];
-    
+
+    static char image[32];
     struct tm *tm;
-    
+
     tm = localtime (&gmt);
-    snprintf (image, sizeof(image), "%2d:%02d", tm->tm_hour, tm->tm_min);
+    snprintf (image, sizeof(image), "%4d.%02d.%02d %2d:%02d",
+		    1900 + tm->tm_year, tm->tm_mon + 1, tm->tm_mday,
+		    tm->tm_hour, tm->tm_min);
 
     return image;
 }
 
+/**
+ * @brief Get some kind of time indication in milliseconds, on Windows it's the time since boot
+ * @brief Get the time since last boot
+ * @return return the time in milliseconds
+ */
 unsigned long roadmap_time_get_millis(void) {
 
    return (unsigned long) GetTickCount();
 
 }
-
-
